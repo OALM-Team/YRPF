381bfe67cf4326bf518ed865a1f5b93c66352b86
Nightwolf <nico.style931@gmail.com>
2020-07-08 10:55:34 +0200

    debug command

--- src/main/java/fr/yuki/YukiRPFramework/commands/DebugTestCommand.java
+++ src/main/java/fr/yuki/YukiRPFramework/commands/DebugTestCommand.java
@@ -8,6 +8,7 @@ import fr.yuki.YukiRPFramework.manager.WorldManager;
 import net.onfirenetwork.onsetjava.entity.Player;
 import net.onfirenetwork.onsetjava.plugin.CommandExecutor;
 
+import java.lang.reflect.Field;
 import java.util.Map;
 
 public class DebugTestCommand implements CommandExecutor {
@@ -22,6 +23,25 @@ public class DebugTestCommand implements CommandExecutor {
             player.sendMessage(job.getKey() + " : (" + job.getValue().getWorldHarvestObjects().size() + ")");
         }
         player.sendMessage("States : (" + CharacterManager.getCharacterStates().size() + ")");
+        player.sendMessage("DelayCount : (" + getDelayCount() + ")");
         return true;
+    }
+
+    private int getDelayCount() {
+        Field f1 = null;
+        try {
+            f1 = Class.forName("net.onfirenetwork.onsetjava.jni.ServerJNI").getDeclaredField("instance");
+            f1.setAccessible(true);
+            Object server = f1.get(null);
+            Field f2 = Class.forName("net.onfirenetwork.onsetjava.jni.ServerJNI").getField("packageBus");
+            Object packageBus = f2.get(server);
+            Field f3 = Class.forName("net.onfirenetwork.onsetjava.jni.PackageBus").getDeclaredField("delayFunctionMap");
+            f3.setAccessible(true);
+            Map<Integer,Runnable> map = (Map<Integer, Runnable>) f3.get(packageBus);
+            return map.size();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 }
